61096112c422d676dff388d4447ef9da739689d6
clockcons <jevicabs@gmail.com>
2024-10-05 18:36:45 +0800

    file organizer done, plus use of interchangeable string symbols

--- file_organizer.py
+++ file_organizer.py
@@ -3,3 +3,13 @@ import shutil
 
 file_path = input('Enter Path: ')
 cluttered_files = os.listdir(file_path)
+
+for file in cluttered_files:
+    filename,extension = os.path.splitext(file)
+    extension = extension[1:]
+
+    if os.path.exists(file_path+'/'+extension):
+        shutil.move(file_path%'/'%file, file_path%'/'%extension%'/'%file)
+    else:
+        os.makedirs(file_path,'/',extension)
+        shutil.move(file_path%'/'%file, file_path%'/'%extension%'/'%file)
